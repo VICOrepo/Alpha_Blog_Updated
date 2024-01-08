41e45d3b3acfb9f52d351b2a859ddb5f89e28c10
VIRAL JAIN <viral.jain@sigmainfo.net>
2024-01-08 13:48:00 +0530

    initial commit

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,61 @@
+class ArticlesController < ApplicationController
+
+    before_action :set_article, only: [:show, :edit, :update, :destroy]
+
+    def show
+        # @article = Article.find(params[:id])
+    end
+
+    def index
+        @articles = Article.all
+    end
+
+    def new
+    @article = Article.new
+    end
+
+    def edit
+        # @article = Article.find(params[:id])
+    end
+
+
+    def create
+      @article = Article.new(article_params)
+      # redirect_to @article
+       if @article.save
+      flash[:notice] = "Article was created successfully."
+      redirect_to @article
+       else
+    # Redirect to the new action to display the form with errors
+        redirect_to new_article_path, alert: @article.errors.full_messages.join(', ')
+       end
+    end
+
+    def update
+        # @article = Article.find(params[:id])
+        if @article.update(article_params)
+        flash[:notice] = "Article was updated successfully."
+        redirect_to @article
+        else
+            redirect_to edit_article_path, alert: @article.errors.full_messages.join(', ')
+         end
+    end
+
+    def destroy
+    # @article = Article.find(params[:id])
+    @article.destroy
+    redirect_to articles_path
+    end
+
+    private
+
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
+
+  
+end
